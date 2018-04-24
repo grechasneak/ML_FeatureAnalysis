4d1cde1ba6eeb0f24f53c496fc1f9d57b1dfb277
Pavel Grechanuk <grechanp@oregonstate.edu>
2018-04-24 14:17:29 -0700

    implemented stratified sampling

--- feature_analysis/data_prep.py
+++ feature_analysis/data_prep.py
@@ -5,34 +5,29 @@ This module will prepare the data for machine learning by scaling it so it has a
 
 '''
 
-def generate_training_data(dataframe):
-	'''
-	Parameters
-	----------
-		dataframe : Pandas DataFrame
-		Should contain all of the cases that will be trained and tested upon.
+import pandas as pd
+import pickle
+from sklearn.model_selection import StratifiedShuffleSplit
 
-	Returns
-	-----------
-	X_train, X_test, X_all, matrix of shape = [n_samples, n_features]
-	y_train, y_test, y_all, array, shape = [n_samples, 1]
-	'''
-	train_set, test_set = train_test_split(dataframe, test_size=0.2, random_state=42)
 
-	X_all = np.vstack(dataframe['s'].values)
-	X_all = np.insert(X_all, 0, dataframe['keig_siml'], axis =1)
+summed_sen_df = pickle.load( open( "../data/sen_data_summed.p", "rb" ) )  
 
-	y_all = np.asarray(dataframe['bias'].values).reshape(-1,1)
-
-	X_train = np.vstack(train_set['s'].values)
-	X_test = np.vstack(test_set['s'].values)
-
-	X_train = np.insert(X_train, 0, train_set['keig_siml'], axis =1)
-	X_test = np.insert(X_test, 0, test_set['keig_siml'], axis =1)
-
-	y_train = np.asarray(train_set['bias'].values).reshape(-1,1)
-	y_test = np.asarray(test_set['bias'].values).reshape(-1,1)
+#This creates a category column so stratified sampling can be done
+category = []
+for i in summed_sen_df.index:
+    category.append(i[:6])
+summed_sen_df['category'] = category	
+summed_sen_df.drop('ieu-comp-therm-002-003', inplace = True)
 	
-	return X_train, X_test, y_train, y_test, X_all, y_all
-
-X_train, X_test, y_train, y_test, X_all, y_all = generate_training_data(summed_sensitivity_df)
+#This function stratifies the data based on the categories in the problem, 
+#so that every type of problem  is sampled representatively. 
+def strat_test_train_split(dataframe):
+    stratifier = StratifiedShuffleSplit(n_splits=4, test_size=0.2, random_state=13)
+    for train_i, test_i in stratifier.split(dataframe, dataframe['category']):
+        
+        test_set = dataframe.iloc[test_i]
+        train_set = dataframe.iloc[train_i]
+    
+    return train_set, test_set
+	
+train, test = strat_test_train_split(summed_sen_df)
