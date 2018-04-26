ac7e99eccc17429d8172d0e644b5d9fed6bcf464
grechasneak <grechanp@oregonstate.edu>
2018-04-25 17:40:23 -0700

    removed test

--- feature_analysis/tests/test_dataload.py
+++ feature_analysis/tests/test_dataload.py
@@ -1,10 +1,9 @@
 
 '''
-This module will prepare the data for machine learning by scaling it so it has a mean of
-0 and a variance of zero.
+This module will test.
 
 '''
 
-from .. import data_prep
+
 
 
