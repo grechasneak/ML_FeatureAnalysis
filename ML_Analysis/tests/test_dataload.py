c70e84c4c15e78910eb5630bb9423558f63f7cfc
grechasneak <grechanp@oregonstate.edu>
2018-05-09 17:03:28 -0700

    trying another way

--- ML_Analysis/tests/test_dataload.py
+++ ML_Analysis/tests/test_dataload.py
@@ -8,6 +8,8 @@ This module will test the data loader.
 import sys, os
 testdir = os.path.dirname(__file__)
 srcdir = '../modules/'
+datadir = '../data/'
+
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 
@@ -19,7 +21,7 @@ from data_prep import *
 
 exclude_list = ['ieu-comp-therm-002-003']
 
-dataframe = load_dataframe('sen_data_summed.p', exclude_list)	
+dataframe = load_dataframe(datasir + 'sen_data_summed.p', exclude_list)	
 
 add_category_column(dataframe)	
 
