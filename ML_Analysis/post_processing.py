a460b6146e2499fa17fdb43e6f7b9c1f178da724
Pavel Grechanuk <grechanp@oregonstate.edu>
2018-05-16 16:03:47 -0700

    adding in models and begining work on post processing module

--- ML_Analysis/post_processing.py
+++ ML_Analysis/post_processing.py
@@ -2,3 +2,9 @@
 This module will contain the code that analyzes the outputs and returns the important features in the problem.
 
 '''
+
+
+def generate_sorted_error(regressor_error, index):
+    rel_errors = tuple(zip(index, regressor_error))
+    rel_errors_sorted = sorted(rel_errors, reverse = True, key = lambda v:v[1])  
+    return rel_errors_sorted
