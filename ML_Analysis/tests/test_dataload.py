fc3279b0f36c74ad6844effe5d86520757607b6e
Pavel Grechanuk <grechanp@oregonstate.edu>
2018-05-21 14:12:46 -0700

    added tests back. Still not working on travis ci.

--- ML_Analysis/tests/test_dataload.py
+++ ML_Analysis/tests/test_dataload.py
@@ -4,54 +4,45 @@ This module will test the data loader.
 
 '''
 
-def asset_test():
-	assert 2 == 2
-
-# import sys, os
-# testdir = os.path.dirname(__file__)
-# srcdir = '../'
-# datadir = '../data/'
-
-# sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
-# from data_prep import *
 
 
-# sys.path.insert(0, os.path.abspath(os.path.join(testdir, datadir)))	
+import sys, os
+import pandas
+testdir = os.path.dirname(__file__)
+srcdir = '../'
+datadir = '../data/'
+
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+from data_prep import *
+
+
 	
-	
-	
-	
-# import sys, os
-# import pkg_resources
-
-
-# testdir = os.path.dirname(__file__)
-# # datadir = '../data/'
-
-# sys.path.insert(0, os.path.abspath(os.path.join(testdir, '/../')))
-
-
-
-# file_path = os.path.join('../data/')
-# filename = pkg_resources.resource_filename(__name__, file_path)
-# sys.path.insert(0, os.path.abspath(filename))
-	
-	
+exclude_list = ['ieu-comp-therm-002-003']	
 	
 	
 
-# exclude_list = ['ieu-comp-therm-002-003']
-
-# dataframe = load_dataframe(datadir + 'sen_data_summed.p', exclude_list)	
-
-# add_category_column(dataframe)	
-
-# stratified_data = strat_test_train_split(dataframe, 'category')
-
-# X_train, X_test, y_train, y_test = generate_training_data(stratified_data, 's', 'bias', True)
-
-
-# def assert_trainingDim():
-	# assert X_train.shape[1] == X_test.shape[1]
 	
-asset_test()
+def test_loading_dataframe():
+	dataframe = load_dataframe(datadir + 'sen_data_summed.p', exclude_list)	
+	assert type(dataframe) == pandas.core.frame.DataFrame
+	return dataframe
+
+
+def assert_trainingDim():
+	assert X_train.shape[1] == X_test.shape[1]
+
+	
+
+
+
+
+#def test_adding_category()
+
+
+dataframe = test_loading_dataframe()
+add_category_column(dataframe)	
+stratified_data = strat_test_train_split(dataframe, 'category')
+X_train, X_test, y_train, y_test = generate_training_data(stratified_data, 's', 'bias', True)
+assert_trainingDim()
+
+
