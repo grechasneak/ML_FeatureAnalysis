1c919c24054ec83b36cbb7e0a4e1fb45b5f48ff3
Pavel Grechanuk <grechanp@oregonstate.edu>
2018-06-10 10:35:29 -0700

    added functions to sort the error and other plotting functions

--- ML_Analysis/post_processing.py
+++ ML_Analysis/post_processing.py
@@ -3,6 +3,26 @@ This module will contain the code that analyzes the outputs and returns the impo
 
 '''
 
+from collections import Counter
+
+def generate_statistics(error):
+    absolute_errors = abs(error)
+    squared_errors = error**2
+    summed_errors = squared_errors.sum()
+    mse = summed_errors/float(len(error))
+    rmse = np.sqrt(mse)
+    mae = absolute_errors.sum()/len(error)
+    print(mse, rmse, mae)
+    return mse, rmse, mae
+
+	
+def gen_sorted_name_error(sorted_err_tuple):
+    sorted_err_names = []
+    sorted_errors = []
+    for i in range(len(sorted_err_tuple)):
+        sorted_err_names.append(sorted_err_tuple[i][0])
+        sorted_errors.append(sorted_err_tuple[i][1])
+    return sorted_err_names, sorted_errors
 
 def generate_sorted_error(regressor_error, index):
 	'''
@@ -19,3 +39,40 @@ def generate_important_features(regressor, feature_index):
 	'''
 	importances = regressor.feature_importances_
 	return list(zip(feature_index, importances))
+	
+	
+
+def count_case_types(index)
+	short_names = []
+	for i in index:
+		short_names.append(i[:7])
+	short_data = Counter(short_names).most_common()
+	return short_data
+	
+def generate_type_errors(index, reg_err, short_data):
+    rel_errors = tuple(zip(index, reg_err))
+    case_errors = []
+    for i, type_case in enumerate(short_data):
+        case_type = type_case[0]
+        errors = []
+        for i, case_error in enumerate(rel_errors):
+            short_name = case_error[0][:7]
+            error = float(case_error[1])
+
+            if short_name == case_type:
+                errors.append(error)
+        case_errors.append(errors)
+        
+        case_errors_array = np.array([np.array(x) for x in case_errors])
+    return case_errors_array
+	
+	
+def gen_type_MSE_MAE_data(reg_err):
+    type_mse = []
+    type_mae = []
+    for i in generate_type_errors(reg_err):
+        mse, rmse, mae = generate_statistics(i)
+        type_mse.append(mse)
+        type_mae.append(mae)    
+    
+    return type_mse, type_mae
--- ML_Analysis/visualization.py
+++ ML_Analysis/visualization.py
@@ -23,3 +23,45 @@ def plot_MSE_hist(error):
     ax2.text(-.033, 100, '$\mu = {:.4e}, \sigma = {:.4f}$'.format(mu, sigma))
 
     plt.show()
+
+	
+def plotError_among_benchType(err_by_type):
+    
+    x_pos = np.arange(len(err_by_type))
+    fig, (ax) = plt.subplots(1, dpi = 160)
+    x = ax.get_xaxis()
+    x.set_ticks(x_pos)
+    x.set_ticklabels([])
+    x.set_ticklabels(names,rotation=80)
+    ax.bar(x_pos[:-1], err_by_type[:-1])
+
+    ax.set_ylabel('MSE')
+    ax.set_title('MSE Among Benchmark Types')
+    plt.show()
+	
+def gen_error_plot(reg_err, regressor, labels):
+    
+    median = np.median(reg_err)
+    mean = np.mean(reg_err)
+
+    case_errors_array = post_processing.generate_type_errors(reg_err)
+    
+    fig, (ax) = plt.subplots(1, dpi = 160)
+    last_point = 0
+    for i, case in enumerate(case_errors_array):
+        ax.scatter(range(last_point, last_point + len(case)), case, label = labels[i])
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        last_point = last_point +len(case)
+
+    xs = np.linspace(1,1100,100)
+    median_data = np.array([median for i in range(len(xs))])
+    mean_data = np.array([mean for i in range(len(xs))])
+
+    ax.plot(xs, median_data, 'r--') 
+    ax.plot(xs, mean_data, 'b-') 
+
+    ax.set_xlabel('Benchmarks')
+    ax.set_ylabel('Error (Prediced - Actual Bias)')
+    ax.set_title('Errors of '+ regressor +' on Predicting Bias')
+    ax.set_ylim([-0.03, 0.03])
+    plt.show()
